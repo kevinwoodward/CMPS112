fa32a2c1ec0161690e704c7227d40ffb9baf41a2
kevinwoodward <keawoodw@ucsc.edu>
2017-10-10 15:25:23 -0700

    let, print, input, and dim working

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -41,7 +41,7 @@
 (define *variable-table* (make-hash))
 
 (define (variable-get key)
-        (hash-ref *variable-table* key))
+        (hash-ref *variable-table* key 0))
 
 (define (variable-put! key value)
         (hash-set! *variable-table* key value))
@@ -54,15 +54,33 @@
 
 ;; Helpers
 
-(define (eval-expr expr)
+(define (get-input-number varname)
+   (display "Input a numeric value for: ")
+   (display varname)
+   (display "\n")
+   (define result (read))
+   (if (not (number? result)) (begin (display "Error: Input not a number! Try again.\n") (get-input-number varname)) result))
+
+(define (error-and-quit filename linenum)
+   (display "Error in file: ")
+   (display filename)
+   (display " on line: ")
+   (display linenum)
+   (display "\n")
+   (exit 1))
+
+;;(define (func-intermediate expr)
+    ;;(if ()
+
+(define (eval-expr expr resolve-vars)
    (cond ((number? expr) expr)
          ((string? expr) expr)
-         ((symbol? expr) (if (variable-has-key? expr) (variable-get expr) expr)
-         ((not (function-has-key? (car expr))) expr)
-         ((pair? expr)   (apply (function-get (car expr))
-                                  (map eval-expr (cdr expr))))
-         (else #f))
-)
+         ((symbol? expr) (if resolve-vars (variable-get expr) expr))
+         ((not (function-has-key? (car expr))) expr)         
+         ((pair? expr)
+               (define res (not (or (eq? (car expr) 'let) (eq? (car expr) 'dim) (eq? (car expr) 'input))))
+               (apply (function-get (car expr))
+               (map (curryr eval-expr res) (cdr expr))))))
 
 ;; Function table declaration
 
@@ -71,18 +89,35 @@
             (function-put! (car pair) (cadr pair)))
     `(
         (print   ,(lambda (arg . rest)
-                      (display arg)
+                      (if (pair? arg) (if (variable-has-key? (car arg)) (display (vector-ref (variable-get (car arg)) (- (cadr arg) 1))) (error "error")) (display arg)) ;; If arg is array ref, get value else print arg                     
                       (display " ")
                       (when (not (null? rest)) (apply (function-get 'print) rest))
                       (when (null? rest) (display "\n"))))
 
         (let     ,(lambda (mem expr)
                       (cond
-                          ((number? expr) (variable-put! mem expr))
+                          ((and (symbol? mem) (number? expr)) (variable-put! mem expr)) ;; Set var to number
+                          ((and (symbol? mem) (symbol? expr) (variable-put! mem (variable-get expr)))) ;; Set var to var
+                          ((and (symbol? mem) (pair? expr)) (variable-put! mem (vector-ref (variable-get (car expr)) (- (cadr expr) 1)))) ;; Set var to array value
+                          ((and (pair? mem) (number? expr)) (vector-set! (variable-get (car mem)) (- (cadr mem) 1) expr)) ;; Set array value to a number 
+                          ((and (pair? mem) (symbol? expr)) (vector-set! (variable-get (car mem)) (- (cadr mem) 1) (variable-get expr))) ;; Set array value to symbol
+                          ((and (pair? mem) (pair? expr)) (vector-set! (variable-get (car mem)) (- (cadr mem) 1) (vector-ref (variable-get (car expr)) (- (cadr expr) 1))))
+                          
                         )))
 
         (dim     ,(lambda (data)
-                      (
+                      (variable-put! (car data) (make-vector (cadr data)))))
+
+        (input   ,(lambda (arg . rest)
+                      (cond
+                          ((symbol? arg) (variable-put! arg (get-input-number arg)))
+                          ((pair? arg)
+                              (if
+                                  (variable-has-key? (car arg))
+                                  (vector-set! (variable-get (car arg)) (- (cadr arg) 1) (get-input-number arg))
+                                  (error))))
+                          (when (not (null? rest)) (apply (function-get 'input) rest)))) 
+                          
         
         (abs     ,abs)
         (acos    ,acos)
@@ -107,7 +142,7 @@
         (^       ,expt)
         (/       ,/)
         (*       ,*)
-        (%       ,(lambda (x y) (- x (* (trunc (/ x y)) y))))
+        (%       ,(lambda (x y) (- x (* (function-get 'trunc (/ x y)) y))))
         (=       ,=)
         (<       ,<)
         (>       ,>)
@@ -167,17 +202,19 @@
                       (label-put! (cadr line) program)))
           (scan-for-labels rest)))
 
-(define (run-program program)
+(define (run-program program filename)
     (when (< 0 (length program))
          (define line (car program))
          (define rest (cdr program))
+         (define linenum (car line))
+         ;;(with-handlers ([exn:fail? (lambda (exn) (error-and-quit filename linenum))]) (interpret-line line))
          (interpret-line line) ;;TODO check at this level if there is a goto, and if so then transfer control by (define rest (label-get labelname))
-         (run-program rest)))
+         (run-program rest filename)))
 
 (define (interpret-line line)
     (cond
-        ((= 3 (length line)) (eval-expr (caddr line)))
-        ((= 2 (length line)) (when (not(label-has-key? (cadr line))) (eval-expr (cadr line))))))
+        ((= 3 (length line)) (eval-expr (caddr line) #t))
+        ((= 2 (length line)) (when (not(label-has-key? (cadr line))) (eval-expr (cadr line) #t)))))
 
 
 (define (main arglist)
@@ -187,6 +224,6 @@
                (program (readlist-from-inputfile sbprogfile)))
               ;;(write-program-by-line sbprogfile program))))
                (scan-for-labels program)
-               (run-program program))))
+               (run-program program sbprogfile))))
 
 (main '("00-hello-world.sbir"))
