bb09e0a5d307b8b0d82aa80a7557af36597e871e
kevinwoodward <keawoodw@ucsc.edu>
2017-12-06 22:40:20 -0800

    Final version

--- asg4/functions.pl
+++ asg4/functions.pl
@@ -1,45 +1,48 @@
 % Kevin Woodward, keawoodw@ucsc.edu
 % Megan Sharp, mesharp@ucsc.edu
 
-mathfns( X, List ) :-
-   S is sin( X ),
-   C is cos( X ),
-   Q is sqrt( X ),
-   List = [S, C, Q].
-
-constants( List ) :-
-   Pi is pi,
-   E is e,
-   Epsilon is epsilon,
-   List = [Pi, E, Epsilon].
-
-sincos( X, Y ) :-
-   Y is sin( X ) ** 2 + cos( X ) ** 2.
+not( X ) :- X, !, fail.
+not( _ ).
 
 get_radians(IATA, RadLat, RadLon):-
-  airport(IATA, _, degmin(A, B), degmin(C, D)),
-  DLatmin is B/60 ,
-  DLonmin is D/60 ,
-  DegreeLat is A + DLatmin,
-  DegreeLon is C + DLonmin,
-  RadLat is DegreeLat * pi/180,
-  RadLon is DegreeLon * pi/180.
+    airport(IATA, _, degmin(A, B), degmin(C, D)),
+    DLatmin is B/60 ,
+    DLonmin is D/60 ,
+    DegreeLat is A + DLatmin,
+    DegreeLon is C + DLonmin,
+    RadLat is DegreeLat * pi/180,
+    RadLon is DegreeLon * pi/180.
 
 haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
-   Dlon is Lon2 - Lon1,
-   Dlat is Lat2 - Lat1,
-   A is sin( Dlat / 2 ) ** 2
-      + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
-   Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
-   Distance is Dist * 3961.
-   not( X ) :- X, !, fail.
-   not( _ ).
-
+    Dlon is Lon2 - Lon1,
+    Dlat is Lat2 - Lat1,
+    A is sin( Dlat / 2 ) ** 2
+        + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
+    Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
+    Distance is Dist * 3961.
 
 haversine_cities(IATA1, IATA2, Distance):-
-  get_radians(IATA1, Lat1, Lon1),
-  get_radians(IATA2, Lat2, Lon2),
-  haversine_radians(Lat1, Lon1, Lat2, Lon2, Distance).
+    get_radians(IATA1, Lat1, Lon1),
+    get_radians(IATA2, Lat2, Lon2),
+    haversine_radians(Lat1, Lon1, Lat2, Lon2, Distance).
+
+% Capital names for airports
+uppercaseairport( 'atl', 'ATL').
+uppercaseairport( 'bos', 'BOS').
+uppercaseairport( 'chi', 'CHI').
+uppercaseairport( 'den', 'DEN').
+uppercaseairport( 'dfw', 'DFW').
+uppercaseairport( 'lax', 'LAX').
+uppercaseairport( 'mia', 'MIA').
+uppercaseairport( 'nyc', 'NYC').
+uppercaseairport( 'sea', 'SEA').
+uppercaseairport( 'sfo', 'SFO').
+uppercaseairport( 'sjc', 'SJC').
+
+flightpossible(_, _, AfterHours, AfterMinutes) :-
+    AfterTime is (AfterHours * 60 ) + AfterMinutes,
+    AfterTime >= 1440,
+    !, fail.
 
 flightpossible(BeforeHours, BeforeMinutes, AfterHours, AfterMinutes) :-
     BeforeTime is (BeforeHours * 60 ) + BeforeMinutes,
@@ -51,19 +54,9 @@ combinetimes(H1, M1, H2, M2, R1, R2) :-
     R1 is T // 60,
     R2 is T mod 60.
 
-writeallpaths( Node, Node ) :-
-  write( Node ), write( ' is ' ), write( Node ), nl.
-
 writeallpaths( Node, Next ) :-
-  listpath( Node, Next, time(0, 0), [Node], List, Times ),
-  writepath( List, Times ).
-
-writeList([]).
-writeList([Head|[]]) :- write(Head).
-
-writeList( [Head|Tail] ) :-
-    write(Head),
-    writeList(Tail).
+    listpath( Node, Next, time(0, 0), [Node], List, Times ),
+    writepath( List, Times ).
 
 writepath( [_|[]], _) :-
   nl.
@@ -73,29 +66,53 @@ writepath( [LocHead|LocTail], [time(Hours, Minutes)|TimeTail]) :-
     TimeTail = [time(NextHours, NextMinutes)|TimesToPass],
     airport(LocHead, From, _, _),
     airport(LocNext, To, _, _),
+    uppercaseairport(LocHead, UpperLocHead),
+    uppercaseairport(LocNext, UpperLocNext),
     %Format: depart  <iata>  <location> <time>
-    format('depart  %s  %s %02d:%02d\n', [LocHead, From, Hours, Minutes]),
-    format('arrive  %s  %s %02d:%02d\n', [LocNext, To, NextHours, NextMinutes]),
+    format('depart  %s  %s %02d:%02d\n',
+        [UpperLocHead, From, Hours, Minutes]),
+    format('arrive  %s  %s %02d:%02d\n',
+        [UpperLocNext, To, NextHours, NextMinutes]),
     writepath( LocTail, TimesToPass  ).
 
 
 %listpath( Node, Node, _, _, [Node|List], _ ). % Done case, old
-listpath( Node, Node, _, _, [Node|_], _ ). % Done case, singleton eliminated
+listpath( Node, Node, _, _, [Node|_], _ ). % Done case
 
-listpath( Node, End, time(PrevHours, PrevMins), Tried, [Node|List], [time(DepHours, DepMinutes),time(NewHours, NewMinutes)|Rest] ) :-
+listpath( Node, End,
+    time(PrevHours, PrevMins), Tried, [Node|List],
+    [time(DepHours, DepMinutes),time(NewHours, NewMinutes)|Rest] ) :-
 
-  flight( Node, Next, time(DepHours, DepMinutes) ),
+    flight( Node, Next, time(DepHours, DepMinutes) ),
 
-  not( member( Next, Tried )),
-  haversine_cities(Node, Next, Distance),
-  ArrivalHours is floor(Distance / 500),
-  ArrivalMinutes is round(((Distance / 500) - ArrivalHours) * 60),
-  combinetimes(DepHours, DepMinutes, ArrivalHours, ArrivalMinutes, NewHours, NewMinutes),
-  flightpossible(PrevHours, PrevMins, DepHours, DepMinutes), %TODO this call not failing when it should
+    not( member( Next, Tried )),
+    haversine_cities(Node, Next, Distance),
+    ArrivalHours is floor(Distance / 500),
+    ArrivalMinutes is round(((Distance / 500) - ArrivalHours) * 60),
+    combinetimes(DepHours, DepMinutes,
+                 ArrivalHours, ArrivalMinutes,
+                 NewHours, NewMinutes),
+    flightpossible(PrevHours, PrevMins, DepHours, DepMinutes),
 
-  listpath( Next, End, time(NewHours, NewMinutes), [Next|Tried], List, Rest ).
+    listpath( Next, End, time(NewHours, NewMinutes),
+              [Next|Tried], List, Rest ).
+
+fly(Source, Source) :-
+    write('Error: Source and destination cannot be identical!\n'),
+    !, fail.
 
 fly(Source, Destination):-
-  airport(Source, _, _, _),
-  airport(Destination, _, _, _),
-  writeallpaths(Source, Destination).
+    airport(Source, _, _, _),
+    airport(Destination, _, _, _),
+    writeallpaths(Source, Destination).
+
+fly(Source, Destination):-
+    airport(Source, _, _, _),
+    airport(Destination, _, _, _),
+    not(writeallpaths(Source, Destination)),
+    write('Path does not exist!\n'),
+    !, fail.
+
+fly(_, _) :-
+    write('Error: Source or destination does not exist!\n'),
+    !, fail.
